c0b8cf83a590e5f6620f2f73a9bcbc37f69952e1
Manolis <manolis-9@hotmail.com>
2024-08-12 23:21:19 +0300

    Fix header styles

--- components/header.tsx
+++ components/header.tsx
@@ -10,17 +10,21 @@ const Header = () => {
     useActiveSectionContext();
 
   return (
-    <header className="z-[999] relative  flex  items-center justify-between md:h-[100px]">
+    <header className="z-[999] fixed lg:sticky top-0 left-0 w-full flex  items-center justify-between md:h-[100px]">
       {/* <motion.div
-        className="fixed top-0 left-0 right-0 ml-auto mr-auto h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-0 ml-auto mr-auto h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         // className=""
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
       ></motion.div> */}
 
-      <div className="flex items-center gap-3">
-        <a href="">Emmanuel Xanthakis</a>
-      </div>
+      <motion.div
+        className="flex items-center gap-3"
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <a href="">Emmanouil Xanthakis</a>
+      </motion.div>
 
       <nav className="flex items-center gap-2">
         {/* <nav className="flex fixed top-[0.15rem] h-12 left-1/2 -translate-x-1/2 mr-auto sm:top-[1.7rem] sm:h-[initial] sm:py-0"> */}
