02b0596fa8a7561ffaf0f6d6e09bfa1bd72d898d
Manolis <manolis-9@hotmail.com>
2024-08-20 01:05:15 +0300

    Change section heading font heading font weight

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const SectionHeading = ({ children, className }: SectionHeadingProps) => {
   return (
     <h2
-      className={`mb-5 text-center md:text-left text-[3rem] md:text-[3.5rem] tracking-tight leading-[120%] font-extrabold w-fit ${className}`}
+      className={`mb-5 text-center md:text-left text-[3rem] md:text-[3.5rem] tracking-tight leading-[120%] font-medium w-fit ${className}`}
     >
       {children}
     </h2>
