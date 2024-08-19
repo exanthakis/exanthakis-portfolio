b49214fef3b7e29a4b1d13a313d5dd97d553d375
Manolis <manolis-9@hotmail.com>
2024-08-20 00:11:24 +0300

    Rework hero cmp

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
     <section
       ref={ref}
       id="home"
-      className="relative mx-auto flex flex-col md:flex-row-reverse justify-center items-center h-[92dvh]"
+      className="relative mx-auto flex flex-col md:flex-row-reverse justify-center items-center h-[92dvh] max-w-3xl text-center"
     >
       {/* <div className="flex items-center justify-center z-10 relative h-3/5"> */}
       {/* <CubeScene /> */}
@@ -34,23 +34,24 @@ const Hero = () => {
         </div> */}
       {/* </div> */}
       <motion.div
-        className="flex flex-col items-start justify-center gap-y-4 px-4 text-lg font-medium"
+        className="flex flex-col items-center justify-center gap-y-8 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
           delay: 0.1,
         }}
       >
-        <h1 className="max-w-md font-black font-calSans bg-white text-left bg-home-hero-title bg-clip-text font-titles text-6xl text-transparent leading-[120%]">
+        <h1 className="font-black font-calSans bg-white bg-home-hero-title bg-clip-text font-titles text-6xl text-transparent leading-[120%]">
           Crafting Innovative Digital Experiences
         </h1>
 
-        <p
-          className={`text-white font-medium lg:text-2xl sm:text-xl xs:text-[20px] text-[16px] lg:leading-[40px] leading-[20px] text-white-100`}
+        <span
+          className={`md:text-xl tracking-[-0.16px] text-[rgba(239,247,255,.615)] font-normal sm:px-20`}
+          // className={`text-white font-medium lg:text-2xl sm:text-xl xs:text-[20px] text-[16px] lg:leading-[40px] leading-[20px] text-white-100`}
         >
           A Frontend developer with <span className="font-bold">3 years</span>{" "}
           of experience in developing user interfaces and web applications.
-        </p>
+        </span>
 
         <Button buttonType={ButtonType.PRIMARY} href="">
           Contact me here{" "}
