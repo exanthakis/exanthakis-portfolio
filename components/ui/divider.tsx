d55755b4c7ee9833a80301dac3a332218297fe28
Manolis Xanthakis <manolis.ksanthakis@gmail.com>
2024-08-25 11:39:23 +0300

    Remove semicolon from inline styles

--- components/ui/divider.tsx
+++ components/ui/divider.tsx
@@ -9,7 +9,7 @@ const Divider = ({ type }: DividerProps) => {
         className="left-1/2 top-0 w-full center pointer-events-none absolute h-px max-w-full -translate-x-1/2 -translate-y-1/2"
         style={{
           background:
-            "linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(255, 255, 255, 0) 0%, rgba(143, 143, 143, 0.67) 50%, rgba(0, 0, 0, 0) 100%);",
+            "linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(255, 255, 255, 0) 0%, rgba(143, 143, 143, 0.67) 50%, rgba(0, 0, 0, 0) 100%)",
         }}
       ></div>
       {type === "light" && (
@@ -18,7 +18,7 @@ const Divider = ({ type }: DividerProps) => {
           className="left-1/2 top-0 h-[300px] w-[80%] center pointer-events-none absolute max-w-full -translate-x-1/2 -translate-y-1/2"
           style={{
             background:
-              "conic-gradient(from 90deg at 50% 50%, rgba(0, 0, 0, 0) 50%, rgb(0, 0, 0) 50%), radial-gradient(rgba(200, 200, 200, 0.1) 0%, transparent 80%);",
+              "conic-gradient(from 90deg at 50% 50%, rgba(0, 0, 0, 0) 50%, rgb(0, 0, 0) 50%), radial-gradient(rgba(200, 200, 200, 0.1) 0%, transparent 80%)",
           }}
         ></div>
       )}
