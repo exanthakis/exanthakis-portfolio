a95006df5e4e31fcfca5d8a987b9c51a1c3af8e5
Manolis Xanthakis <manolis.ksanthakis@gmail.com>
2024-08-28 22:13:42 +0300

    Added overflow hidden to x axis at main layout page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Projects from "@/components/projects/projects";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-6 md:px-4 mx-auto">
+    <main className="flex flex-col items-center px-6 md:px-4 mx-auto overflow-x-hidden">
       <Hero />
       <About />
       <Projects />
