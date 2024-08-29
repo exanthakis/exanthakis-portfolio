6c246384e86431de86520e4442cdfcbb3d86b428
Manolis Xanthakis <manolis.ksanthakis@gmail.com>
2024-08-29 18:02:11 +0300

    Edit filters component

--- components/projects/filter.tsx
+++ components/projects/filter.tsx
@@ -1,36 +1,70 @@
 import { FilterProps, TechStackE } from "@/lib/types";
 import React from "react";
 
+const FilterBadge = ({
+  activeTechStack,
+  currentStack,
+}: {
+  activeTechStack: TechStackE;
+  currentStack: TechStackE;
+}) => {
+  return (
+    <div
+      className={`${
+        activeTechStack === currentStack ? "bg-[#68cc58]" : "bg-[#1c1c1c]"
+      } w-1 h-6 rounded-full transition duration-500`}
+    ></div>
+  );
+};
+
+const FilterButton = ({
+  activeTechStack,
+  currentStack,
+  onFilterClick,
+}: {
+  activeTechStack: TechStackE;
+  currentStack: TechStackE;
+  onFilterClick: (id: TechStackE) => void;
+}) => {
+  return (
+    <button
+      className={`flex gap-3 transition duration-500 ${
+        activeTechStack === currentStack
+          ? "text-white font-medium"
+          : "text-[#62666d] font-normal"
+      }`}
+      onClick={() => onFilterClick(currentStack)}
+    >
+      <FilterBadge
+        activeTechStack={activeTechStack}
+        currentStack={currentStack}
+      />
+      <span className="">{currentStack}</span>
+    </button>
+  );
+};
+
 const Filter = ({ activeTechStack, onActiveTechStach }: FilterProps) => {
   const handleFilterClick = (id: TechStackE) => {
     onActiveTechStach(id);
   };
   return (
-    <div className="flex gap-4 flex-col items-start justify-center pb-8">
-      <button
-        className={`${
-          activeTechStack === TechStackE.All ? "text-pink-400" : ""
-        }`}
-        onClick={() => handleFilterClick(TechStackE.All)}
-      >
-        All
-      </button>
-      <button
-        className={`${
-          activeTechStack === TechStackE.REACT ? "text-pink-400" : ""
-        }`}
-        onClick={() => handleFilterClick(TechStackE.REACT)}
-      >
-        React
-      </button>
-      <button
-        className={`${
-          activeTechStack === TechStackE.HTML ? "text-pink-400" : ""
-        }`}
-        onClick={() => handleFilterClick(TechStackE.HTML)}
-      >
-        HTML/CSS
-      </button>
+    <div className="flex gap-4 flex-col items-start justify-center pb-8 w-fit">
+      <FilterButton
+        activeTechStack={activeTechStack}
+        currentStack={TechStackE.All}
+        onFilterClick={handleFilterClick}
+      />
+      <FilterButton
+        activeTechStack={activeTechStack}
+        currentStack={TechStackE.REACT}
+        onFilterClick={handleFilterClick}
+      />
+      <FilterButton
+        activeTechStack={activeTechStack}
+        currentStack={TechStackE.HTML}
+        onFilterClick={handleFilterClick}
+      />
     </div>
   );
 };
