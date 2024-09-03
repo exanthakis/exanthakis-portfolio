017047760db68a03c20f84d742dcc3de6630eea8
Manolis Xanthakis <manolis.ksanthakis@gmail.com>
2024-09-03 15:40:21 +0300

    Fix experience sticky behavior

--- components/experience.tsx
+++ components/experience.tsx
@@ -30,7 +30,10 @@ const Experience = () => {
           const month = monthNames[date.getMonth()];
           const year = date.getFullYear();
           return (
-            <li key={experience.id} className="relative flex w-full flex-row">
+            <li
+              key={experience.id}
+              className="relative flex w-full flex-row overflow-hidden sm:overflow-visible"
+            >
               <div className="flex w-[7.375rem] sm:w-[12.5rem] pb-4 ">
                 <p className="text-sm leading-[1.6] text-[#f1f7feb5] font-normal">
                   <time className="sticky top-24" dateTime={experience.date}>
@@ -38,7 +41,7 @@ const Experience = () => {
                   </time>
                 </p>
               </div>
-              <div className="relative flex w-[3rem] sm:w-[9.375rem]">
+              <div className="relative flex w-[3rem] sm:w-[9.375rem] overflow-visible">
                 <div className="sticky left-0 top-[102px] mt-1.5 h-1.5 w-1.5 rounded-full bg-[#f1f7feb5]"></div>
                 <div className="absolute left-0.5 top-0.5 h-full w-0.5 bg-[#d6ebfd30]"></div>
               </div>
