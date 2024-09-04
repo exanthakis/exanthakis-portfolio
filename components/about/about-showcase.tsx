c73b4a7f703b482c97841d0a4667eb537a1af295
Manolis Xanthakis <manolis.ksanthakis@gmail.com>
2024-09-04 20:12:55 +0300

    About | Rework text

--- components/about/about-showcase.tsx
+++ components/about/about-showcase.tsx
@@ -9,41 +9,35 @@ const AboutShowcase = () => {
       <SectionHeading>About</SectionHeading>
       <div className="flex flex-col md:flex-row w-full gap-4 md:gap-0 pt-7">
         <h3 className="w-full md:w-1/2 flex items-start justify-start text-4xl">
-          Heading title lorem ipsum
+          Hi I&apos;m Emmanouel
         </h3>
         <div className="w-full md:w-1/2 flex flex-col items-start justify-center text-sm">
           <AboutParagraph id={1}>
-            A Front-End developer with{" "}
-            <span className="font-medium">over 3 years</span> of experience in
-            building large-scale applications, focused on driving performance,
-            reliability, and scalability of customer-facing web applications.
-            Skilled in <span className="font-medium">problem-solving</span> and
-            executing software tasks from start to finish. Capable of working
-            independently or as part of a team, with high levels of organization
-            to consistently meet deadlines.
+            Graduate with a{" "}
+            <span className="font-medium">Master&apos;s degree</span> in
+            Electrical and Computer Engineering from the Technical University of
+            Crete, I specialize in creating seamless and intuitive user
+            experiences. I&apos;m involved in every step of the process: from
+            development to testing and deployment. I focus on delivering
+            high-quality, scalable solutions that drive positive user
+            experiences.
           </AboutParagraph>
 
           <AboutParagraph id={2}>
-            Graduate with a degree in{" "}
-            <span className="font-medium">Accounting</span>, I decided to pursue
-            my passion for programming. I enrolled in a coding bootcamp and
-            learned{" "}
-            <span className="font-medium">full-stack web development</span>.{" "}
-            <span className="italic">My favorite part of programming</span> is
-            the problem-solving aspect. I{" "}
-            <span className="underline">love</span> the feeling of finally
-            figuring out a solution to a problem. My core stack is{" "}
-            <span className="font-medium">React, Next.js</span>. I am also
-            familiar with TypeScript. I am always looking to learn new
-            technologies.
+            I typically use a combination of{" "}
+            <span className="font-medium">
+              JavaScript, React, Redux, Webpack, and GitHub Actions
+            </span>
+            . I have a keen eye for design and am meticulous about continuously
+            learning and improving my skills. Currently, I am learning Three.js
+            to create interactive 3D experiences on the web.
           </AboutParagraph>
-          <AboutParagraph id={3}>
+
+          <AboutParagraph id={2}>
             <span className="italic">When I&apos;m not coding</span>, I enjoy
-            playing video games, watching movies, and playing with my dog. I
-            also enjoy <span className="font-medium">learning new things</span>.
-            I am currently learning about{" "}
-            <span className="font-medium">history and philosophy</span>.
-            I&apos;m also learning how to play the guitar.
+            hiking, watching movies, and traveling to explore new cultures. I
+            also love reading about emerging technologies and spending time
+            outdoors.
           </AboutParagraph>
         </div>
       </div>
