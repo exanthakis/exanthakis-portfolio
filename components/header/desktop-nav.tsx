f0cde10304d108f7fd553c592468c75d5155eca2
Manolis Xanthakis <manolis.ksanthakis@gmail.com>
2024-09-05 00:27:58 +0300

    Trigger deploy in prod

--- components/header/desktop-nav.tsx
+++ components/header/desktop-nav.tsx
@@ -9,7 +9,7 @@ const DesktopNav = () => {
     useActiveSectionContext();
 
   return (
-    <nav className="hidden md:flex items-center gap-2 ">
+    <nav className="hidden md:flex items-center gap-2">
       <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
         {links.map((link) => (
           <li
