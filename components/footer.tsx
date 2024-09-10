ae4ad072193d2e84e43f9345c01f0dd365329fe6
Manolis Xanthakis <manolis.ksanthakis@gmail.com>
2024-09-10 21:27:04 +0300

    Center footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       className="relative h-24 px-4 text-center text-gray-500"
       style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%" }}
     >
-      <div className="fixed h-24 w-full bottom-0 flex flex-col justify-center">
+      <div className="fixed h-24 w-full bottom-0 left-0 flex flex-col justify-center items-center">
         <small className="mb-2 block text-xs">
           &copy; {new Date().getFullYear()} {fullName} All rights reserved
         </small>
