b4ee91e522d89d012137515da8f4ed9c69b43b89
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-07-15 14:12:29 +0300

    Badge | Reduce gap from title

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Badge = ({ className, title }: BadgeProps) => {
   return (
-    <div className="flex flex-row items-center gap-3 pb-4">
+    <div className="flex flex-row items-center gap-2 pb-4">
       <div className={`h-[0.5rem] w-[0.875rem] rounded-full ${className}`}></div>
       {title && <span className="text-left text-sm font-bold text-[#d0d6e0]">{title}</span>}
     </div>
