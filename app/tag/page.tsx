dbab117234d7d4fa350960b50ac2d2653961fda9
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-11-06 16:35:09 +0200

    Fix tags padding

--- app/tag/page.tsx
+++ app/tag/page.tsx
@@ -25,12 +25,12 @@ export default async function TagsPage() {
   return (
     <section className="mx-auto max-w-5xl bg-black px-4 pt-24 text-left md:max-w-4xl md:px-10 md:py-16 lg:max-w-6xl">
       <div className="flex flex-col items-start justify-start divide-y divide-gray-200 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0 dark:divide-gray-700">
-        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+        <div className="space-x-2 pb-2 pt-6 sm:pb-8 md:space-y-5">
           <h1 className="md:leading-14 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl dark:text-gray-100">
             Tags
           </h1>
         </div>
-        <div className="flex max-w-lg flex-wrap">
+        <div className="flex max-w-lg flex-wrap pt-4 sm:pt-0">
           {result.length === 0 && "No tags found."}
           {result.map((tag) => {
             return (
